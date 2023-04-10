8a29cba768f52622109d6264e27fb1d9597eb5c5
Fareez <fare4z@gmail.com>
2023-04-10 23:02:19 +0800

    Add project files.

---
+++ Animal.vb
@@ -0,0 +1,53 @@
+﻿Public Class Animal
+    Public Property Name As String
+    Public Property Age As Integer
+    Public Sub Sleep()
+        MsgBox(Name & " is sleeping.")
+    End Sub
+
+    Public Overridable Sub Speak()
+        MsgBox(Name & " makes a noise.")
+    End Sub
+End Class
+
+Public Class Dog
+    Inherits Animal
+
+    Public Function PictureSpeak() As Bitmap
+        PictureSpeak = My.Resources.dog_bark
+    End Function
+
+    Public Function PictureSleep() As Bitmap
+        PictureSleep = My.Resources.dog_sleep
+    End Function
+    Public Sub Sound()
+        Dim player As New System.Media.SoundPlayer(My.Resources.dog_barking_6296)
+        player.Play()
+        MsgBox(Name & " is barking.")
+    End Sub
+
+    Public Overrides Sub Speak()
+        MsgBox(Name & " says woof.")
+    End Sub
+End Class
+
+Public Class Cat
+    Inherits Animal
+    Public Sub Sound()
+        Dim player As New System.Media.SoundPlayer(My.Resources.nonino_new_141718)
+        player.Play()
+        MsgBox(Name & " is meowing.")
+    End Sub
+
+    Public Overrides Sub Speak()
+        MsgBox(Name & " says meow.")
+    End Sub
+
+    Public Function PictureSpeak() As Bitmap
+        PictureSpeak = My.Resources.cat_meow
+    End Function
+
+    Public Function PictureSleep() As Bitmap
+        PictureSleep = My.Resources.cat_sleep
+    End Function
+End Class
---
+++ Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim myAnimal As New Animal
+        myAnimal.Name = TextBox1.Text
+        myAnimal.Age = TextBox2.Text
+
+        myAnimal.Sleep()
+        myAnimal.Speak()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim myDog As New Dog
+        myDog.Name = TextBox1.Text
+        myDog.Age = TextBox2.Text
+        myDog.Sleep()
+        PictureBox1.Image = myDog.PictureSleep
+        myDog.Sound()
+        PictureBox1.Image = myDog.PictureSpeak
+        myDog.Speak()
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim myCat As New Cat
+        myCat.Name = TextBox1.Text
+        myCat.Age = TextBox2.Text
+        myCat.Sleep()
+        PictureBox1.Image = myCat.PictureSleep
+        myCat.Sound()
+        PictureBox1.Image = myCat.PictureSpeak
+        myCat.Speak()
+    End Sub
+
+
+End Class
